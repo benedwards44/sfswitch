04f0395ca9091fbf0612d1269ff458853e4b5fa7
Ben Edwards <ben@edwards.nz>
2016-08-26 08:05:04 +1200

    Hey

--- sfswitch/urls.py
+++ sfswitch/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView, RedirectView
 from django.contrib import admin
-admin.autodiscover()
+admin.autodiscover() 
 
 urlpatterns = patterns('',
     url(r'^$', 'enable_disable.views.index', name='index'),
