40c6cc8466b0ef886948459c86f151ec5f166e51
Ben Edwards <ben@edwards.nz>
2023-04-24 10:36:49 +1200

    Move brackets

--- enable_disable/tasks.py
+++ enable_disable/tasks.py
@@ -281,7 +281,7 @@ def get_metadata(job_id):
 
 				# Save the zip file result to server
 				zip_file = open('metadata.zip', 'w+')
-				zip_file.write(b64decode(retrieve_result.zipFile.decode('utf-8')))
+				zip_file.write(b64decode(retrieve_result.zipFile).decode('utf-8'))
 				zip_file.close()
 
 				# Open zip file
